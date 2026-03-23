02984201ff6d838b861da9b1251156a287a90667
VINH_DEV <jy.shjn@gmail.com>
2026-03-23 10:53:11 +0700

    Enhance Walks retrieval with sorting and pagination
    Updated the `GetAll` method in `WalksController` to support
    additional query parameters: `sortBy`, `isAscending`,
    `pageNumber`, and `pageSize`. Modified the `IWalksRepository`
    interface and implemented the new method signature in
    `SQLWalksRepository` to include sorting and pagination logic,
    allowing for more flexible data retrieval.

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -38,11 +38,14 @@ namespace NZWalks.API.Controllers
         }
 
         //Get Walks
-        // Get: /api/walks?filterOn=Name&filterQuery=track&sortBy=Name&isAscending=true&pageNumber=1&pageSize=100
+        // Get: /api/walks?filterOn=Name&filterQuery=track&sortBy=Name&isAscending=true&PageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery,
+            [FromQuery] string? sortBy, [FromQuery] bool? isAscending,
+            [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000)
         {
-            var walksDomainModel = await walksRepository.GetAllAsync(filterOn, filterQuery);
+            var walksDomainModel = await walksRepository.GetAllAsync(filterOn, filterQuery, sortBy,
+                isAscending ?? true, pageNumber, pageSize);
             //Map Domain model to DTO
             return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
         }
--- NZWalks.API/Repositories/SQLWalksRepository.cs
+++ NZWalks.API/Repositories/SQLWalksRepository.cs
@@ -31,7 +31,8 @@ namespace NZWalks.API.Repositories
             return existingWalk;
         }
 
-        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null)
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
+            string? sortBy = null, bool isAscending = true, int pageNumber = 1, int pageSize = 1000)
         {
             var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
 
@@ -43,7 +44,24 @@ namespace NZWalks.API.Repositories
                     walks = walks.Where(w => w.Name.Contains(filterQuery));
                 }
             }
-            return await walks.ToListAsync();
+
+            // Sorting
+            if (string.IsNullOrWhiteSpace(sortBy) == false)
+            {
+                if (sortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(w => w.Name) : walks.OrderByDescending(w => w.Name);
+                }
+                else if (sortBy.Equals("Length", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(w => w.LengthInKm) : walks.OrderByDescending(w => w.LengthInKm);
+                }
+            }
+
+            // Pagination
+            var skipResults = (pageNumber - 1) * pageSize;
+
+            return await walks.Skip(skipResults).Take(pageSize).ToListAsync();
             //return await dbContext.Walks.Include("Difficulty").Include("Region").ToListAsync();
         }
 
