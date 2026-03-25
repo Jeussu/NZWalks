3128ad28fcfb3b0e29dcf6bc0331b74e6df74fbc
VINH_DEV <jy.shjn@gmail.com>
2026-03-25 11:57:23 +0700

    Add ASP.NET Core Identity for user authentication
    Introduce `NZWalksAuthDbContext` for managing user roles and authentication. Add migration `CreatingAuthDatabase` to define identity-related tables and seed initial roles. Update model snapshot to reflect new schema. This sets up a basic authentication system with role-based access control.

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,33 @@ namespace NZWalks.API.Data
     {
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
+        }
+
+        override protected void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var readerRoleId = "7518b19c-304c-44ee-81d2-58d59582362e";
+            var writerRoleId = "f5b5b19c-304c-44ee-81d2-58d59582362e";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
