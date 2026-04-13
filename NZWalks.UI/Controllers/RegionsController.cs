771c3223c254176ca11694710daddc5ca182c5db
002586 <Vinh140698>
2026-04-13 10:27:10 +0700

    Add Edit and Delete region functionality to UI
    Implemented Edit (GET/POST) and Delete actions in RegionsController. Added Edit.cshtml view with edit and delete form. Updated Index.cshtml to include Edit button for each region. Enables editing and deleting regions from the UI.

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -64,5 +64,63 @@ namespace NZWalks.UI.Controllers
             }
             return View(respose);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var response = await client.GetFromJsonAsync<RegionDto>($"https://localhost:7032/api/regions/{id.ToString()}");
+            if (response is not null)
+            {
+                return View(response);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionDto request)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7032/api/regions/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Edit", "Regions");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7032/api/regions/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+                // Console
+            }
+            return View("Edit");
+        }
     }
 }
